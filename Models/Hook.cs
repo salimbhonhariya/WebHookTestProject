311dcedc53677755db1ad14081979e8d85bd5550
Abdul Bhonhariya <Abdul Bhonhariya>
2016-07-20 22:31:50 -0400

    First Commit

---
+++ Models/Hook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HookWebAPI.Models
+{
+    public class Hook
+    {
+        int id;
+    }
+}
